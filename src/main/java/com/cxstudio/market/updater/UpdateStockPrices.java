42b8649bc7712e1272da163cef231ea3c0f21012
cxstudio-com <topgun117@gmail.com>
2014-06-19 22:39:24 -0700

    Fix start time

--- src/main/java/com/cxstudio/market/updater/UpdateStockPrices.java
+++ src/main/java/com/cxstudio/market/updater/UpdateStockPrices.java
@@ -16,17 +16,20 @@ import com.cxstudio.market.updater.persistent.TradeDao;
 
 public class UpdateStockPrices implements Runnable {
 	static final long ALMOST_A_DAY = 23 * 60 * 60 * 1000; // 23 hours
+	static final int MARKET_STARTS_HOUR = 6; // 6AM
+	static final int MARKET_END_HOUR = 13; // 1PM
 	static Logger log = Logger.getLogger(UpdateStockPrices.class.getName());
 	private final SymbolDao symbolDao;
 	private final TradeDao tradeDao;
 	private final DataProvider dataProvider;
 	private final UpdateResult updateResult;
+	private final Date todayMarketStart;
+	private final Date todayMarketEnd;
 
 	public static void main(String[] args) throws Exception {
 		SymbolDao symbolDao = new SymbolDao();
 		TradeDao tradeDao = new TradeDao();
 		DataProvider dataProvider = new GoogleFinanceDataRetreiver();
-
 		UpdateStockPrices updateStockPrices = new UpdateStockPrices(symbolDao, tradeDao, dataProvider);
 
 		log.info("Stock update started.");
@@ -43,12 +46,22 @@ public class UpdateStockPrices implements Runnable {
 		this.tradeDao = tradeDao;
 		this.dataProvider = dataProvider;
 		this.updateResult = new UpdateResult();
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, MARKET_STARTS_HOUR);
+		this.todayMarketStart = cal.getTime();
+		cal.set(Calendar.HOUR_OF_DAY, MARKET_END_HOUR);
+		this.todayMarketEnd = cal.getTime();
 	}
 
 	@Override
 	public void run() {
 		List<Symbol> symbolsToUpdate = null;
-		updateResult.startTime = System.currentTimeMillis();
+		updateResult.startTime = new Date();
+		if (!isTimeToRun(updateResult.startTime)) {
+			log.warn("Not time to run yet. Current time: " + updateResult.startTime + " Market end time: "
+					+ this.todayMarketEnd);
+			return;
+		}
 		try {
 			symbolDao.connect();
 			tradeDao.connect();
@@ -63,11 +76,10 @@ public class UpdateStockPrices implements Runnable {
 				filter.setInterval(30);
 				int counter = 1;
 				for (Symbol symbol : symbolsToUpdate) {
-					try {
-						log.info("Updating # " + (counter++) + " of " + symbolsToUpdate.size() + ": "
-								+ symbol.getTicker());
-						if (System.currentTimeMillis() - symbol.getLastUpdate().getTime() > ALMOST_A_DAY) {
-							// last update is more than a day, then update
+					if (needsUpdate(symbol.getLastUpdate())) {
+						try {
+							log.info("Updating # " + (counter++) + " of " + symbolsToUpdate.size() + ": "
+									+ symbol.getTicker());
 							List<Trade> trades = dataProvider.retreive(symbol, filter);
 							if (trades != null && trades.size() > 0) {
 								tradeDao.insertTrades(trades);
@@ -77,13 +89,14 @@ public class UpdateStockPrices implements Runnable {
 								updateResult.numOfTradesUpdated += trades.size();
 							}
 							log.info(trades.size() + " num of trades retreived for: " + symbol.getTicker());
-						} else { // last update is less than a day, skip
-							log.info("Skipping " + symbol.getTicker() + ". Last updated: " + symbol.getLastUpdate());
+						} catch (Exception e) {
+							log.error("Problem inserting trades for " + symbol.getTicker() + " into DB.");
+							updateResult.exception = e;
+							updateResult.successful = false;
 						}
-					} catch (Exception e) {
-						log.error("Problem inserting trades for " + symbol.getTicker() + " into DB.");
-						updateResult.exception = e;
-						updateResult.successful = false;
+					} else {
+						log.info("Stock " + symbol.getTicker() + " is up to date. Last update: "
+								+ symbol.getLastUpdate());
 					}
 				}
 				dataProvider.disconnect();
@@ -96,16 +109,24 @@ public class UpdateStockPrices implements Runnable {
 			symbolDao.disconnect();
 			tradeDao.disconnect();
 		}
-		updateResult.endTime = System.currentTimeMillis();
+		updateResult.endTime = new Date();
 	}
 
 	public UpdateResult getUpdateResult() {
 		return updateResult;
 	}
 
+	private boolean isTimeToRun(Date timeToRun) {
+		return timeToRun.after(this.todayMarketEnd);
+	}
+
+	private boolean needsUpdate(Date lastUpdate) {
+		return lastUpdate.before(this.todayMarketStart);
+	}
+
 	private static class UpdateResult {
-		long startTime;
-		long endTime;
+		Date startTime;
+		Date endTime;
 		int numOfStocksToUpdate;
 		int numOfStocksUpdated;
 		long numOfTradesUpdated;
@@ -113,13 +134,13 @@ public class UpdateStockPrices implements Runnable {
 		Exception exception;
 
 		long getDuration() {
-			return endTime - startTime;
+			return endTime.getTime() - startTime.getTime();
 		}
 
 		@Override
 		public String toString() {
-			return "UpdateResult [startTime=" + new Date(startTime) + ", endTime=" + new Date(endTime) + ", duration="
-					+ getDuration() / 1000 + ", numOfStocksToUpdate=" + numOfStocksToUpdate + ", numOfStocksUpdated="
+			return "UpdateResult [startTime=" + startTime + ", endTime=" + endTime + ", duration=" + getDuration()
+					/ 1000 + ", numOfStocksToUpdate=" + numOfStocksToUpdate + ", numOfStocksUpdated="
 					+ numOfStocksUpdated + ", numOfTradesUpdated=" + numOfTradesUpdated + ", successful=" + successful
 					+ ", exception=" + exception + "]";
 		}
